16fba8cd010af3b24676999ce14cddde075a1613
Serghei Vlaicu <82963033+vlaicu89@users.noreply.github.com>
2021-05-22 07:34:58 -0500

    hello feature started

--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -2,5 +2,7 @@ public class TestJava {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing");
+        System.out.println("GitHub repo is already published");
+        System.out.println("do more work");
     }
 }
